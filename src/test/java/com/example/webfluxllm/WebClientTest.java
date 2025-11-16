8dcd8db73dd45a51b6409f677872546c130e4921
yuhwanwoo <6514687@naver.com>
2025-11-16 15:54:27 +0900

    webclient 구독 예시

---
+++ src/test/java/com/example/webfluxllm/WebClientTest.java
@@ -0,0 +1,34 @@
+package com.example.webfluxllm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+@SpringBootTest
+public class WebClientTest {
+
+    private WebClient webClient = WebClient.builder().build();
+
+    @Test
+    public void testWebClient() {
+        Flux<Integer> intFlux = webClient.get()
+                        .uri("http://localhost:8080/reactive/onenine/flux")
+                                .retrieve()
+                                        .bodyToFlux(Integer.class);
+
+        intFlux.subscribe(data -> {
+                    System.out.println("처리되고 있는 스레이 이름: " + Thread.currentThread().getName());
+                    System.out.println("webFlux가 구동 중!! : " + data);
+                }
+
+        );
+        System.out.println("Netty 이벤트 루프로 스레드 복귀 !!");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+
+        }
+    }
+}
